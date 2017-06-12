933ecc222ffe46b3dc10a21bdf9fa2702ab948d2
Derrick Park <park@Derricks-MacBook-Pro.local>
2017-06-12 09:15:32 -0700

    code

---
+++ Day19/CriminalMind/app/src/main/java/com/derrick/park/criminalmind/CrimePagerActivity.java
@@ -0,0 +1,21 @@
+package com.derrick.park.criminalmind;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by park on 2017-06-09.
+ */
+
+public class CrimePagerActivity extends AppCompatActivity {
+    // Will create and manage the ViewPager
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crime_pager);
+
+
+    }
+}
